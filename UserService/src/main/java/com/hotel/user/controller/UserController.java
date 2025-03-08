27631376dbf5e042ac219949076110399771c865
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2025-03-08 12:10:05 +0530

    Implemented Circuit Breaker

--- UserService/src/main/java/com/hotel/user/controller/UserController.java
+++ UserService/src/main/java/com/hotel/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.hotel.user.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hotel.user.dto.UserDto;
 import com.hotel.user.service.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@PostMapping("/create")
 	public ResponseEntity<UserDto> createNewUser(@RequestBody UserDto userDto){
@@ -33,10 +39,18 @@ public class UserController {
 	}
 	
 	@GetMapping("/{userId}")
+	@CircuitBreaker(name = "userService", fallbackMethod = "userFallback")
 	public ResponseEntity<UserDto> getUserDataById(@PathVariable String userId){
+		logger.info("Getting Single User Data: Inside getUserDataById()");
 		return ResponseEntity.ok(this.userService.getUserById(userId));
 	}
 	
+	// Fallback method when circuit breaker is OPEN
+    public ResponseEntity<String> userFallback(String userId, Throwable ex) {
+        logger.error("Fallback method executed due to: {}", ex.getMessage());
+        return ResponseEntity.ok("Downstream Service is Down");
+    }
+	
 	//Update Data for User
-	//Delete a user
+	//Delete a User
 }
