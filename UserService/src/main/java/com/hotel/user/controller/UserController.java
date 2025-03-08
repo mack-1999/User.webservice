898675efe00f9ab700a56dd7c0c8c3bcc2126788
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2025-03-08 15:28:33 +0530

    Implemented RateLimiter & Retry

--- UserService/src/main/java/com/hotel/user/controller/UserController.java
+++ UserService/src/main/java/com/hotel/user/controller/UserController.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,8 @@ import com.hotel.user.dto.UserDto;
 import com.hotel.user.service.UserService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("/users")
@@ -25,6 +29,8 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	int retryCount = 1;
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -39,9 +45,14 @@ public class UserController {
 	}
 	
 	@GetMapping("/{userId}")
-	@CircuitBreaker(name = "userService", fallbackMethod = "userFallback")
+	@Retry(name = "userServiceRetry", fallbackMethod = "userFallback")
+	//@CircuitBreaker(name = "userServiceCircuitBreaker", fallbackMethod = "userFallback")
+	//@RateLimiter(name = "userServiceRateLimiter", fallbackMethod = "userFallbackRateLimiter")
 	public ResponseEntity<UserDto> getUserDataById(@PathVariable String userId){
 		logger.info("Getting Single User Data: Inside getUserDataById()");
+		//logger.info("Retry Count:{}",retryCount);
+		//retryCount++;
+		
 		return ResponseEntity.ok(this.userService.getUserById(userId));
 	}
 	
@@ -49,6 +60,12 @@ public class UserController {
     public ResponseEntity<String> userFallback(String userId, Throwable ex) {
         logger.error("Fallback method executed due to: {}", ex.getMessage());
         return ResponseEntity.ok("Downstream Service is Down");
+    }
+    
+    // Fallback method when circuit breaker is OPEN
+    public BodyBuilder userFallbackRateLimiter(String userId, Throwable ex) {
+        logger.error("Fallback method executed due to: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS);
     }
 	
 	//Update Data for User
